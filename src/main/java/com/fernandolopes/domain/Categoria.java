ca4744417d4ae8de879b14f85e57d13ecefa5e59
FernandoLopes <nandolopis@gmail.com>
2017-12-28 22:59:25 -0200

    Projeto iniciado, criado classe Entity

---
+++ src/main/java/com/fernandolopes/domain/Categoria.java
@@ -0,0 +1,91 @@
+package com.fernandolopes.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	//set variaveis privadas
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	@ManyToMany(mappedBy="Categoria")
+	private List<Produto> list = new ArrayList<>();
+	
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	
+	public List<Produto> getList() {
+		return list;
+	}
+
+	public void setList(List<Produto> list) {
+		this.list = list;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
