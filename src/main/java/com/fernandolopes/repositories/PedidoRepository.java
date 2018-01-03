05db921809641dea7140bdd6b5e85c578d60c396
Fernando Lopes <nandolopis@gmail.com>
2018-01-03 14:23:01 -0200

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/fernandolopes/repositories/PedidoRepository.java
+++ src/main/java/com/fernandolopes/repositories/PedidoRepository.java
@@ -1,11 +1,20 @@
 package com.fernandolopes.repositories;
 
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fernandolopes.domain.Cliente;
 import com.fernandolopes.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true) //nao necessita se uma transação de banco de dados, deixando mais rapido resuzindo o lock
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/fernandolopes/services/PedidoService.java
+++ src/main/java/com/fernandolopes/services/PedidoService.java
@@ -3,8 +3,13 @@ package com.fernandolopes.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.fernandolopes.domain.Categoria;
+import com.fernandolopes.domain.Cliente;
 import com.fernandolopes.domain.ItemPedido;
 import com.fernandolopes.domain.PagamentoComBoleto;
 import com.fernandolopes.domain.Pedido;
@@ -14,51 +19,49 @@ import com.fernandolopes.repositories.ItemPedidoRepository;
 import com.fernandolopes.repositories.PagamentoRepository;
 import com.fernandolopes.repositories.PedidoRepository;
 import com.fernandolopes.repositories.ProdutoRepository;
+import com.fernandolopes.security.UserSS;
+import com.fernandolopes.services.exception.AuthorizationException;
 import com.fernandolopes.services.exception.ObjectNotFoundException;
-
-
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
+
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Pedido.class.getName());
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
 		}
 		return obj;
 	}
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
@@ -71,10 +74,20 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.save(obj.getItens());
-		
+
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
-		
+
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
