81f7c5bf4fe0fee45c2d08e673d7a71ddf7ff37a
Fernando Lopes <nandolopis@gmail.com>
2020-10-23 22:19:04 -0300

    no message

--- src/main/java/com/fernandolopes/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/fernandolopes/cursomc/repositories/CidadeRepository.java
@@ -16,5 +16,7 @@ public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 	@Transactional(readOnly=true)
 	@Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
 	public List<Cidade> findCidades(@Param("estadoId") Integer estado_id);
+	
+	
 
 }
